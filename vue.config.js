66913dfe1ec34e7c3bcd374c3f33a765d455f2d2
Aldhair Vera Camacho <u201315853@upc.edu.pe>
2020-05-22 19:31:02 -0500

    Trying to deploy

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 // vue.config.js
 module.exports = {
   publicPath: "/WebPortfolio/",
-  productionSourceMap: false,
-  assetsDir: "../static",
-  outputDir: "../static",
-  indexPath: "../templates/index.html"
+  outputDir: "dist/",
 };
